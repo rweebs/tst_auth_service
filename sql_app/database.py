80b8a053b3b82234828b8085bf27af05e4f43107
rweebs <67186137+rweebs@users.noreply.github.com>
2021-11-14 21:59:32 +0700

    fix database

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_USER = "bowo"
 DATABASE_PASSWORD = "password"
 DATABASE_URI = "128.199.151.55"
-DATABASE_NAME = "startup"
+DATABASE_NAME = "rpl"
 SQLALCHEMY_DATABASE_URL = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_URI}:3306/{DATABASE_NAME}"
 
 engine = create_engine(
